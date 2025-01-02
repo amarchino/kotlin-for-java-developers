c10154225428cdcb40bc17cab2cb106f2b7ec416
amarchino <marchino.alessandro@gmail.com>
2025-01-02 23:58:24 +0100

    Lesson 30

--- src/main/java/academy/learnprogramming/datatypes/javacode/DummyClass.java
+++ src/main/java/academy/learnprogramming/datatypes/javacode/DummyClass.java
@@ -4,4 +4,9 @@ public class DummyClass {
     public String isVacationTime(boolean onVacation) {
         return onVacation ? "I'm on vacation" : "I'm working";
     }
+    public void printNumbers(int[] numbers) {
+        for(int number : numbers) {
+            System.out.println(number);
+        }
+    }
 }
--- src/main/kotlin/academy/learnprogramming/datatypes/Datatypes.kt
+++ src/main/kotlin/academy/learnprogramming/datatypes/Datatypes.kt
@@ -1,6 +1,7 @@
 package academy.learnprogramming.datatypes
 
 import academy.learnprogramming.datatypes.javacode.DummyClass
+import java.math.BigDecimal
 
 fun main() {
     val myInt = 10
@@ -33,4 +34,43 @@ fun main() {
     val anything: Any
     val unit: Unit
     val nothing: Nothing
+
+    val names = arrayOf("John", "Jane", "Jill", "Joe")
+    val longs1 = arrayOf(1L, 2L, 3L)
+    val longs2 = arrayOf<Long>(1, 2, 3, 4)
+    val longs3 = arrayOf(1, 2, 3, 4)
+    println(longs1 is Array<Long>)
+    println(longs2 is Array<Long>)
+    println(longs3 is Array<Int>)
+
+    println(longs1[2])
+
+    val evenNumbers = Array(16) { it * 2 }
+    for (number in evenNumbers) {
+        println(number)
+    }
+    val lotsOfNumbers = Array(100_000) { it + 1 }
+    val allZeroes = Array(100) { 0 }
+
+    var someArray: Array<Int>
+    someArray = arrayOf(1, 2, 3, 4)
+    someArray = Array(6) { (it + 1) * 10 }
+    for (number in someArray) {
+        println(number)
+    }
+
+    val mixedArray = arrayOf("hello", 22, BigDecimal(10.5), 'a')
+    for (element in mixedArray) {
+        println(element)
+    }
+    val myIntArray = intArrayOf(3, 9, 434, 2, 33)
+    DummyClass().printNumbers(myIntArray)
+
+    //var someOtherArray = Array<Int>(5)
+    var someOtherArray = IntArray(5)
+    for (number in someOtherArray) {
+        println(number)
+    }
+    DummyClass().printNumbers(evenNumbers.toIntArray())
+    val convertedIntArray = myIntArray.toTypedArray()
 }
