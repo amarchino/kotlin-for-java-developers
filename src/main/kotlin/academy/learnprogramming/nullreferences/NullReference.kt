efbc89765c14d4e1a3e0737f08bae63c84dfe3de
amarchino <marchino.alessandro@gmail.com>
2025-01-04 23:49:18 +0100

    Lesson 32

--- src/main/kotlin/academy/learnprogramming/nullreferences/NullReference.kt
+++ src/main/kotlin/academy/learnprogramming/nullreferences/NullReference.kt
@@ -18,4 +18,16 @@ fun main() {
     val str3 = something as? String
     println(str3)
     println(str3?.uppercase())
+
+    val str1: String? = "This isn't null"
+    str1?.let { printText(it) }
+    val str4 = str1!!.uppercase()
+
+    val str5: String? = null
+    val anotherString: String = "This isn't nullable"
+    println(str5 == anotherString)
+}
+
+fun printText(text: String) {
+    println(text)
 }
