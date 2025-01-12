a519cadc72f4d3513a583309d40af6c39efc6744
amarchino <marchino.alessandro@gmail.com>
2025-01-12 20:55:27 +0100

    Lesson 37

--- src/main/kotlin/academy/learnprogramming/accessmodifier/AccessModifier.kt
+++ src/main/kotlin/academy/learnprogramming/accessmodifier/AccessModifier.kt
@@ -1,10 +1,27 @@
 package academy.learnprogramming.accessmodifier
 
 fun main() {
-    val emp = Employee();
-    println(emp)
+    val emp = Employee("John");
+    println(emp.firstName)
+
+    val emp2 = Employee("Joe")
+    println(emp2.firstName)
+    println(emp2.fullTime)
+
+    val emp3 = Employee("Jane", false)
+    println(emp3.firstName)
+    println(emp3.fullTime)
+
+    println(Demo().dummy)
 }
 
-private class Employee {
+class Employee (val firstName: String, val fullTime: Boolean = true) {
 
+}
+
+class Demo {
+    val dummy: String
+    constructor() {
+        dummy = "Hello"
+    }
 }
