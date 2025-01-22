62d3847cd788844f4cdfabc8f525502d84d17099
amarchino <marchino.alessandro@gmail.com>
2025-01-22 20:34:45 +0100

    Lesson 39

--- src/main/kotlin/academy/learnprogramming/accessmodifier/AccessModifier.kt
+++ src/main/kotlin/academy/learnprogramming/accessmodifier/AccessModifier.kt
@@ -1,6 +1,10 @@
 package academy.learnprogramming.accessmodifier
 
+val MY_CONSTANTS = 100
+
 fun main() {
+    println(MY_CONSTANTS)
+
     val emp = Employee("John");
     emp.fullTime = false
     println(emp.firstName)
@@ -15,6 +19,19 @@ fun main() {
     println(emp3.fullTime)
 
     println(Demo().dummy)
+
+    val car = Car("blue", "Toyota", 2015)
+    println(car)
+    val car2 = Car("blue", "Toyota", 2015)
+    println(car == car2)
+    val car3 = car.copy()
+    println(car3)
+    val car4 = car.copy(year = 2016, color = "green")
+    println(car4)
+}
+
+data class Car(val color: String, val model: String, val year: Int) {
+
 }
 
 class Employee (val firstName: String, fullTime: Boolean = true) {
