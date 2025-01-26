9e764a7c05fda0b7d789346d16201839f884f984
amarchino <marchino.alessandro@gmail.com>
2025-01-26 19:23:46 +0100

    Lesson 40

---
+++ src/main/kotlin/academy/learnprogramming/datatypes/Functions.kt
@@ -0,0 +1,39 @@
+package academy.learnprogramming.datatypes
+
+fun main() {
+    println(labelMultiply(label = "Here's the result:", operand2 = 3, operand1 = 4))
+    val emp = Employee("Jane")
+    println(emp.uppercaseFirstName())
+
+    val car1 = Car("blue", "Toyota", 2015)
+    val car2 = Car("red", "Ford", 2016)
+    val car3 = Car("grey", "Ford", 2017)
+    printColors(car1, car2, car3, str = "Color: ")
+
+    val manyCars = arrayOf(car1, car2, car3)
+    printColors(*manyCars, str = "Test: ")
+
+    val moreCars = arrayOf(car2, car3)
+    val car4 = car2.copy()
+    val lotsOfCars = arrayOf(*manyCars, *moreCars, car4)
+    for(c in lotsOfCars) {
+        println(c)
+    }
+}
+
+fun labelMultiply(operand1: Int, operand2: Int, label: String = "The answer is:") =
+    "$label ${operand1 * operand2}"
+
+fun whatever() = 3 * 4
+
+fun printColors (vararg cars: Car, str: String) {
+    for(car in cars) {
+        println(car.color)
+    }
+}
+
+class Employee(val firstName: String) {
+    fun uppercaseFirstName() = firstName.uppercase()
+}
+
+data class Car(val color: String, val model: String, val year: Int)
