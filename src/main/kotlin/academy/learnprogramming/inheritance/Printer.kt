35c974439e85cdf1ee3d33a39b470b7f5b92a132
amarchino <marchino.alessandro@gmail.com>
2025-01-28 22:34:12 +0100

    Lesson 44

--- src/main/kotlin/academy/learnprogramming/inheritance/Printer.kt
+++ src/main/kotlin/academy/learnprogramming/inheritance/Printer.kt
@@ -1,8 +1,10 @@
 package academy.learnprogramming.inheritance
 
 fun main() {
-    var laserPrinter = LaserPrinter("Brother 1234")
+    var laserPrinter = LaserPrinter("Brother 1234", 15)
     laserPrinter.printModel()
+
+    SomethingElse("Whatever", 1)
 }
 
 abstract class Printer(val modelName: String) {
@@ -10,8 +12,27 @@ abstract class Printer(val modelName: String) {
     abstract fun bestSellingPrice(): Double
 }
 
-class LaserPrinter(modelName: String): Printer(modelName) {
+open class LaserPrinter(modelName: String, val ppm: Int): Printer(modelName) {
 
-    override fun printModel() = println("The model name of this laser printer is $modelName")
+    final override fun printModel() = println("The model name of this laser printer is $modelName")
     override fun bestSellingPrice() = 129.99
 }
+
+class SpecialLaserPrinter(modelName: String): LaserPrinter(modelName, 100) {
+    // override fun printModel() = println("this is my way of doing it")
+}
+
+open class Something(val x: Int) {
+    var someProperty: String = "something"
+    constructor(someParameter: String, y: Int): this(y) {
+        someProperty = someParameter
+        println("I'm in the parent's constructor")
+    }
+}
+class SomethingElse: Something {
+    constructor(someOtherParameter: String, y: Int): super(someOtherParameter, y) {
+        println("I'm in the child's constructor")
+    }
+}
+
+data class DataClass(val number: Int)
