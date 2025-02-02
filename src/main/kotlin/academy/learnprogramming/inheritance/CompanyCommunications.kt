af7fa5fad5f3314e88db41b1c2bcbd0692a58c23
amarchino <marchino.alessandro@gmail.com>
2025-02-02 19:36:05 +0100

    Lesson 47

--- src/main/kotlin/academy/learnprogramming/inheritance/CompanyCommunications.kt
+++ src/main/kotlin/academy/learnprogramming/inheritance/CompanyCommunications.kt
@@ -5,10 +5,31 @@ import java.time.Year
 fun main() {
     println(CompanyCommunications.getTagLine())
     println(CompanyCommunications.getCopyrightLine())
+    println(SomeClass.accessPrivateVar())
+
+    val someClass1 = SomeClass.justAssign("this is the string as is")
+    val someClass2 = SomeClass.upperOrLowerCase("This isn't the string as is", false)
+    println(someClass1.someString)
+    println(someClass2.someString)
 }
 
 object CompanyCommunications {
     val currentYear = Year.now().value
     fun getTagLine() = "Out company rocks!"
     fun getCopyrightLine() = "Copyright \u00A9 $currentYear Our Company. All rights reserved"
+}
+
+class SomeClass private constructor(val someString: String) {
+    companion object {
+        private val privateVal = 0
+        fun accessPrivateVar() = "I'm accessing privateVar: $privateVal"
+        fun justAssign(str: String) = SomeClass(str)
+        fun upperOrLowerCase(str: String, lowerCase: Boolean): SomeClass {
+            return if(lowerCase) {
+                SomeClass(str.lowercase())
+            } else {
+                SomeClass(str.uppercase())
+            }
+        }
+    }
 }
