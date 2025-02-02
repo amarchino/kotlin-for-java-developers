95df2f667f5701526514c42e701def002a952a75
amarchino <marchino.alessandro@gmail.com>
2025-02-02 18:54:44 +0100

    Lesson 45

--- src/main/kotlin/academy/learnprogramming/inheritance/Printer.kt
+++ src/main/kotlin/academy/learnprogramming/inheritance/Printer.kt
@@ -12,21 +12,33 @@ abstract class Printer(val modelName: String) {
     abstract fun bestSellingPrice(): Double
 }
 
-open class LaserPrinter(modelName: String, val ppm: Int): Printer(modelName) {
+open class LaserPrinter(modelName: String, val ppm: Int): Printer(modelName), MyInterface {
+    override val number: Int = 1
 
     final override fun printModel() = println("The model name of this laser printer is $modelName")
     override fun bestSellingPrice() = 129.99
+    override fun myFunction(str: String): String {
+        TODO("Not yet implemented")
+    }
 }
 
 class SpecialLaserPrinter(modelName: String): LaserPrinter(modelName, 100) {
     // override fun printModel() = println("this is my way of doing it")
 }
 
-open class Something(val x: Int) {
+open class Something(val x: Int): MySubInterface {
     var someProperty: String = "something"
+    override val number: Int = 25
+    override val number2 = 12
     constructor(someParameter: String, y: Int): this(y) {
         someProperty = someParameter
         println("I'm in the parent's constructor")
+    }
+    override fun mySubFunction(num: Int): String {
+        TODO("Not yet implemented")
+    }
+    override fun myFunction(str: String): String {
+        TODO("Not yet implemented")
     }
 }
 class SomethingElse: Something {
@@ -36,3 +48,13 @@ class SomethingElse: Something {
 }
 
 data class DataClass(val number: Int)
+
+interface MyInterface {
+    val number: Int
+    val number2: Int
+        get() = 45
+    fun myFunction(str: String): String
+}
+interface MySubInterface: MyInterface {
+    fun mySubFunction(num: Int): String
+}
