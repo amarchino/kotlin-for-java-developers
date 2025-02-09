5f195a28e3758243fb1cc9a738e1e719839b8160
amarchino <marchino.alessandro@gmail.com>
2025-02-09 21:56:36 +0100

    Lesson 53

--- src/main/kotlin/academy/learnprogramming/challenge/Challenge3.kt
+++ src/main/kotlin/academy/learnprogramming/challenge/Challenge3.kt
@@ -1,10 +1,31 @@
 package academy.learnprogramming.challenge
 
 fun main() {
+    val bicycle = KotlinBicycle(10, 3, 10)
+    bicycle.printDescription()
 
+    val mountainBike = KotlinMountainBicycle(20, 10, 10, 30)
+    mountainBike.printDescription()
+
+    val roadBike = KotlinRoadBicycle(10, 10, 10, 3)
+    roadBike.printDescription()
+
+    val bicycle1 = KotlinBicycle(10, 3)
+    bicycle1.printDescription()
+
+    val mountainBike1 = KotlinMountainBicycle(20, 10, 10)
+    mountainBike1.printDescription()
+
+    val roadBike1 = KotlinRoadBicycle(10, 10, 10)
+    roadBike1.printDescription()
+
+    val mountainBike2 = KotlinMountainBicycle("blue", 10, 10, 10)
+    mountainBike2.printDescription()
+
+    KotlinMountainBicycle.availableColors.forEach { println(it) }
 }
 
-open class KotlinBicycle(var cadence: Int, var gear: Int, var speed: Int) {
+open class KotlinBicycle(var cadence: Int, var speed: Int, var gear: Int = 10) {
     fun applyBrake(decrement: Int) {
         this.speed -= decrement
     }
@@ -15,15 +36,19 @@ open class KotlinBicycle(var cadence: Int, var gear: Int, var speed: Int) {
 
     open fun printDescription() = println("Bike is in gear $gear with a cadence of $cadence travelling at a speed of $speed.")
 }
-class KotlinMountainBicycle(cadence: Int, gear: Int, speed: Int, var seatHeight: Int): KotlinBicycle(cadence, gear, speed) {
-    // Nothing
+class KotlinMountainBicycle(var seatHeight: Int, cadence: Int, speed: Int, gear: Int = 10): KotlinBicycle(cadence, speed, gear) {
+    constructor(color: String, seatHeight: Int, cadence: Int, speed: Int, gear: Int = 20): this(seatHeight, cadence, speed, gear) {
+        println("This is the $color")
+    }
     override fun printDescription() {
         super.printDescription()
         println("The mountain bike has a seat height of $seatHeight inches.")
     }
+    companion object {
+        val availableColors = listOf("blue", "red", "white", "black", "green", "yellow")
+    }
 }
-class KotlinRoadBicycle(cadence: Int, gear: Int, speed: Int, val tireWidth: Int): KotlinBicycle(cadence, gear, speed) {
-    // Nothing
+class KotlinRoadBicycle(val tireWidth: Int, cadence: Int, speed: Int, gear: Int = 10): KotlinBicycle(cadence, speed, gear) {
     override fun printDescription() {
         super.printDescription()
         println("The road bike has a tire width of $tireWidth mm.")
