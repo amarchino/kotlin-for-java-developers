516cb5ab2e5411d8c06b7871e0587ac12eebcb77
amarchino <marchino.alessandro@gmail.com>
2025-02-18 22:28:32 +0100

    Lesson 62

--- src/main/kotlin/academy/learnprogramming/section7/Lambda.kt
+++ src/main/kotlin/academy/learnprogramming/section7/Lambda.kt
@@ -1,6 +1,8 @@
 package academy.learnprogramming.section7
 
 fun main() {
+    println(countTo100())
+
     // run { println("I'm in a lambda") }
     val employees = listOf(
         Employee("John", "Smith", 2020),
@@ -8,6 +10,9 @@ fun main() {
         Employee("Mary", "Johnson", 2010),
         Employee("Mike", "Jones", 2002)
     )
+    findByLastName(employees, "Wilson")
+    findByLastName(employees, "Smithson")
+
     // println(employees.minBy { e -> e.startYear })
     // println(employees.minBy { e: Employee -> e.startYear })
     // println(employees.minBy { it.startYear })
@@ -18,8 +23,33 @@ fun main() {
         num += 15
         println(num)
     }
+    "Some String".apply somestring@ {
+        "Another String".apply {
+            println(lowercase())
+            println(this@somestring.uppercase())
+        }
+    }
 
     run(::topLevel)
+}
+
+fun countTo100() = StringBuilder().apply {
+    for(i in 1..99) {
+        append(i)
+        append(",")
+    }
+    append(100)
+}.toString()
+
+fun findByLastName(employees: List<Employee>, lastName: String) {
+    employees.forEach returnBlock@ {
+        if(it.lastName == lastName) {
+            println("Yes, there's an employee with the last name $lastName")
+            return
+            //return@returnBlock
+        }
+    }
+    println("Nope, there's no employee with the last name $lastName")
 }
 
 fun topLevel() = println("I'm in a function")
