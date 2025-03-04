ac989ed72548710294b65ca5f976721b8f3eb05b
amarchino <marchino.alessandro@gmail.com>
2025-03-04 21:53:01 +0100

    Lesson 82

--- src/main/kotlin/academy/learnprogramming/section9/CallJava.kt
+++ src/main/kotlin/academy/learnprogramming/section9/CallJava.kt
@@ -5,6 +5,18 @@ fun main() {
     car.color = "green"
     println(car)
 
+    car.variableMethod(5, "Hello", "Goodbye")
+    val strings = arrayOf("Hello", "Goodbye")
+    car.variableMethod(5, *strings)
+
+    car.wantsIntArray(intArrayOf(1, 2, 3))
+
+    // (car.anObject as java.lang.Object).notify()
+    println("x = ${Car.x}")
+    println("x = ${Car.xString()}")
+
+    car.demoMethod { println("I'm in a thread") }
+
     var model: String? = car.model
     println(model?.javaClass)
 
