eed86c069bf350974068cd35a53f6364a2922d1f
amarchino <marchino.alessandro@gmail.com>
2025-03-05 19:26:43 +0100

    Lesson 84

--- src/main/java/academy/learnprogramming/section9/CallKotlinFromJava.java
+++ src/main/java/academy/learnprogramming/section9/CallKotlinFromJava.java
@@ -1,5 +1,7 @@
 package academy.learnprogramming.section9;
 
+import java.io.IOException;
+
 public class CallKotlinFromJava {
     public static void main(String[] args) {
         // KotlinCarCodeKt.topLevel();
@@ -13,6 +15,22 @@ public class CallKotlinFromJava {
         System.out.println(car.getColor());
         System.out.println(car.isAutomatic());
 
+        // KotlinCar.Companion.carComp();
+        KotlinCar.carComp();
+        // SingletonObj.INSTANCE.doSomething();
+        SingletonObj.doSomething();
+        //System.out.println("isAuto = " + KotlinCar.Companion.isAuto());
+        System.out.println("isAuto = " + KotlinCar.isAuto());
+        System.out.println("Constant field: " + KotlinCar.constant);
 
+        //car.printMe(null);
+        try {
+            StaticCar.doIO();
+        } catch (IOException e) {
+            System.out.println("IO Exception");
+        }
+
+        StaticCar.defaultArgs("The number is : ", 40);
+        StaticCar.defaultArgs("With default value!");
     }
 }
--- src/main/kotlin/academy/learnprogramming/section9/KotlinCarCode.kt
+++ src/main/kotlin/academy/learnprogramming/section9/KotlinCarCode.kt
@@ -2,10 +2,21 @@
 
 package academy.learnprogramming.section9
 
+import java.io.IOException
+import kotlin.jvm.Throws
+
 fun topLevel() = println("I'm in the KotlinCarCode file!")
 
 fun main() {
     "Print this".print()
+}
+
+@JvmOverloads fun defaultArgs(str: String, num: Int = 25) {
+
+}
+
+@Throws(IOException::class) fun doIO() {
+    throw IOException()
 }
 
 class KotlinCar(color: String, @JvmField val model: String, val year: Int, val isAutomatic: Boolean) {
@@ -13,6 +24,19 @@ class KotlinCar(color: String, @JvmField val model: String, val year: Int, val i
         set(value) {
             field = "always green"
         }
+
+    companion object {
+        const val constant = 25
+        @JvmStatic val isAuto = false
+        @JvmStatic fun carComp() = println("I'm in KotlinCar companion object")
+    }
+
+    fun printMe(text: String) {
+        println("I don't expect a null value: $text")
+    }
+}
+object SingletonObj {
+    @JvmStatic fun doSomething() = println("I'm doing something in the singoleton object")
 }
 
 fun String.print() {
